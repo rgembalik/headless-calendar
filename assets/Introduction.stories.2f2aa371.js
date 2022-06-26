7c3d57dcb4dfe904b8fcfa1ec5c86f1e94fecf53
rgembalik <rgembalik@users.noreply.github.com>
2022-06-26 20:43:59 +0000

    Deploying to gh-pages from @ rgembalik/headless-calendar@ba06d2889578e1459736c05f6d52c6b1173e0d62 🚀

---
+++ assets/Introduction.stories.2f2aa371.js
@@ -0,0 +1,24 @@
+var n={parameters:{storySource:{source:`export default {\r
+  title: "Introduction",\r
+};\r
+\r
+export const Introduction = () => {\r
+  const div = document.createElement("div");\r
+  div.innerHTML = \`\r
+    <div style="font-family: system-ui;">\r
+        <h1>Headless Calendar</h1>\r
+\r
+        <p>Hi! Welcome to storybook for Headless Calendar. Please select one of the stories for specific framework in the menu on the left.</p>\r
+    </div>\r
+\`;\r
+\r
+  return div;\r
+};\r
+`,locationsMap:{introduction:{startLoc:{col:28,line:5},endLoc:{col:1,line:16},startBody:{col:28,line:5},endBody:{col:1,line:16}}}}},title:"Introduction"};const t=()=>{const e=document.createElement("div");return e.innerHTML=`
+    <div style="font-family: system-ui;">
+        <h1>Headless Calendar</h1>
+
+        <p>Hi! Welcome to storybook for Headless Calendar. Please select one of the stories for specific framework in the menu on the left.</p>
+    </div>
+`,e},o=["Introduction"];export{t as Introduction,o as __namedExportsOrder,n as default};
+//# sourceMappingURL=Introduction.stories.2f2aa371.js.map
